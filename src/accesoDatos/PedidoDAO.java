62b1f55a0a7673c9836a7585cd4f8d782a804802
CrissCandioti <141766978+CrissCandioti@users.noreply.github.com>
2023-10-03 21:11:33 -0300

    Metodos para Guardia y pedido
    Se agregaron los metodos

--- src/accesoDatos/PedidoDAO.java
+++ src/accesoDatos/PedidoDAO.java
@@ -7,6 +7,8 @@ package accesoDatos;
 
 import entidades.Pedido;
 import javax.swing.JOptionPane;
+import service.EnfermoService;
+import service.GuardiaService;
 
 /**
  *
@@ -20,6 +22,22 @@ public final class PedidoDAO extends DAO {
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Se produjo un error al intentar guardar el pedido en la base de datos");
         }
+    }
+
+    public Pedido buscarPedidoPorID(int id) {
+        try {
+            String sql = "SELECT `idPedido`, `fechaPedido`, `idGuardia`, `idEnfermo` FROM `pedido` WHERE idPedido = " + id;
+            consultarBaseDatos(sql);
+            GuardiaService gs = new GuardiaService();
+            EnfermoService ef = new EnfermoService();
+            Pedido aux = null;
+            while (resultado.next()) {                
+                
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Se produjo un error al intentar buscar el pedido en la base de datos");
+        }
+        return null;
     }
 
 }
--- src/service/GuardiaService.java
+++ src/service/GuardiaService.java
@@ -5,10 +5,33 @@
  */
 package service;
 
+import accesoDatos.GuardiaDAO;
+import entidades.Guardia;
+import java.time.LocalDate;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author criss
  */
 public class GuardiaService {
-    
+
+    public void crearGuardia(LocalDate fecha, int idVocal, int idTelefonista, int idChofer, int idAcompañante, int idSacerdote, int idPedido) {
+        try {
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "No se pudo registrar correctamente la guardia");
+        }
+    }
+
+    public Guardia buscarGuardiaPorID(int id) {
+        try {
+            GuardiaDAO dao = new GuardiaDAO();
+            return dao.buscarGuardiaPorID(id);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "No se pudo encontrar la Guardia solicitada");
+        }
+        return null;
+
+    }
 }
